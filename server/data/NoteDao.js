f1aff1222b9f213556e046dddc7045276ad9eec3
sge0208 <geruijia0208@gmail.com>
2022-07-03 20:49:17 -0400

    edit NoteDao

--- server/data/NoteDao.js
+++ server/data/NoteDao.js
@@ -6,25 +6,53 @@ class NoteDao {
         this.notes = [];
     }
 
+    // Pre: title and text are not undefined, and title is not empty
     async create ({ title, text }) {
+        const note = new Note(title, text);
+        this.notes.push(note);
+        return note;
 
     }
 
-    async update (id, { title, text }) {
+// Pre: id is a valid note ID
+    async update(id, { title, text }) {
+        const index = this.notes.findIndex((note) => note._id === id);
 
+        if (title !== undefined) {
+            this.notes[index].title = title;
+        }
+
+        if (text !== undefined) {
+            this.notes[index].text = text;
+        }
+
+        return this.notes[index];
     }
 
-    async delete (id) {
 
+// Pre: id is a valid note ID
+    async delete(id) {
+        const index = this.notes.findIndex((note) => note._id === id);
+        const note = this.notes[index];
+        this.notes.splice(index, 1);
+        return note;
     }
 
-    async read (id) {
-
+// Pre: id is a valid note ID
+    async read(id) {
+        return this.notes.find((note) => note._id === id);
     }
 
-    async readAll (query = "") {
 
+    async readAll(query = "") {
+        if (query !== "") {
+            return this.notes.filter(
+                (note) => note.title.includes(query) || note.text.includes(query)
+            );
+        }
+        return this.notes;
     }
+
 }
 
 module.exports = NoteDao;
