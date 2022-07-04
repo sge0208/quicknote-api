3c5b00050c6d9f6576b882a08c4dd8853b034d56
sge0208 <geruijia0208@gmail.com>
2022-07-03 21:05:39 -0400

    add fake data

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const faker = require("faker");
+const NoteDao = require("./data/NoteDao");
+
+const NUM_SAMPLES = 3;
+const notes = new NoteDao();
+for (let i = 0; i < NUM_SAMPLES; i++) {
+  notes.create({
+    title: faker.lorem.sentence(),
+    text: faker.lorem.paragraph(),
+  });
+}
+
+
 
 app.get("/", (req, res) => {
   res.send("QuickNote API!");
